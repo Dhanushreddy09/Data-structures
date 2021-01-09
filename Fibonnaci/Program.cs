6c9cfbb757e2dccd5d8c0e7df1b0c8567dea40c5
Dhanush reddy <Dhanushreddy09@users.noreply.github.com>
2021-01-09 22:26:33 +0530

    Numericals

---
+++ Fibonnaci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace factorial
+{
+    class fibonacci{
+        public fibonacci(int num){
+if(num<=0){
+    throw new ArgumentException("fibonacci series is not possible for numbers less than or equal to 0");
+}
+   var prev=0;
+   var current=1;
+Console.Write(prev);
+   while(num!=0){
+       Console.Write(",");
+       Console.Write(current);
+var next=prev+current;
+prev=current;
+current=next;
+num--;
+   }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find out the fibonacci series");
+            string x=Console.ReadLine();
+            int y=Convert.ToInt32(x);
+            fibonacci series=new fibonacci(y);
+        }
+    }
+}
---
+++ factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find out factorial for :");
+            string x=Console.ReadLine();
+            int y=Convert.ToInt32(x);
+            int fact=factorial(y);
+            Console.WriteLine(fact);
+            static int factorial(int num){
+                if(num<0){
+                    throw new ArgumentException("factorial cannot be calculated for numbers less than zero");
+                }
+           return num==0?1:num*factorial(num-1);
+            }
+        }
+    }
+}
